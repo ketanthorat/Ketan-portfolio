978ab757b6eb80b9d8b94df118381b33d7888191
dhruv0485 <dhruvmehta.2004@gmail.com>
2025-10-23 16:08:12 +0530

    new changes

--- src/components/gallery/MasonryGrid.tsx
+++ src/components/gallery/MasonryGrid.tsx
@@ -98,7 +98,6 @@ export default function MasonryGrid() {
                                 priority={index < 10}
                                 sizes="(max-width: 560px) 100vw, (max-width: 1024px) 50vw, (max-width: 1440px) 33vw, 25vw"
                                 radius="m"
-                                aspectRatio={image.orientation === "horizontal" ? "16 / 9" : "9 / 16"}
                                 src={image.src}
                                 alt={image.alt}
                                 className={styles.gridItem}
